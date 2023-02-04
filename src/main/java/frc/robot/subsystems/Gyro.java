c52720914c079fae059b07ea64ba01fd6d1ff591
An Qi Ke <kea@bxscience.edu>
2023-02-04 16:41:18 -0500

    added navx library

---
+++ src/main/java/frc/robot/subsystems/Gyro.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.kauailabs.navx.frc.AHRS
+
+public class Gyro extends SubsystemBase {
+  private final AHRS gyro = new AHRS(SPI.Port.kMXP);
+  /** Creates a new Gyro. */
+  public Gyro() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
