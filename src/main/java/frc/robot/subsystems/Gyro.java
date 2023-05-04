ce6ae3be18991ee099c4f8821a3c992cac86f7b5
stacyc2449 <chens18@bxscience.edu>
2023-05-03 23:18:18 -0400

    added print outs

--- src/main/java/frc/robot/subsystems/Gyro.java
+++ src/main/java/frc/robot/subsystems/Gyro.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.kauailabs.navx.frc.AHRS;
 
@@ -24,5 +25,8 @@ public class Gyro extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.getNumber("angle", gyro.getAngle());
+    System.out.println(gyro.getAngle());
+    System.out.println("Raw axis: " + gyro.getRawGyroZ());
   }
 }
